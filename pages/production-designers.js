a77432750d0d3809e62588bb4487705fb3871e18
Scrabby2196 <spam2196@gmail.com>
2025-04-23 20:30:02 +0530

    Create production-designers.js

---
+++ pages/production-designers.js
@@ -0,0 +1,13 @@
+export default function ProductionDesigners() {
+  return (
+    <div>
+      <h1>Production Designers</h1>
+      <p>Community-rated directory of production designers</p>
+      {/* Later, this will be dynamic from Supabase */}
+      <ul>
+        <li>Designer A - ⭐⭐⭐⭐</li>
+        <li>Designer B - ⭐⭐⭐</li>
+      </ul>
+    </div>
+  )
+}
