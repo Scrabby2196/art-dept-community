dc46289a9593083c2052497274d88c16972e4426
Scrabby2196 <spam2196@gmail.com>
2025-04-23 20:30:26 +0530

    Create jobs.js

---
+++ pages/jobs.js
@@ -0,0 +1,12 @@
+export default function Jobs() {
+  return (
+    <div>
+      <h1>Job Listings</h1>
+      <p>Share and explore job opportunities for art department professionals</p>
+      <ul>
+        <li>Set Designer needed for Mumbai shoot - May 1</li>
+        <li>Prop Master for commercial ad - May 5</li>
+      </ul>
+    </div>
+  )
+}
